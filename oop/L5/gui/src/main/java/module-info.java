32e6709dcf1a4f2466ead6a33a7a3d0571368a36
Paweł Grzegory <78054688+xXShaneXx@users.noreply.github.com>
2024-05-30 17:51:30 +0200

    Delete oop/L5/gui/src/main/java/module-info.java

--- oop/L5/gui/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.gui {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.desktop;
-
-
-    opens com.example.gui to javafx.fxml;
-    exports com.example.gui;
-}
