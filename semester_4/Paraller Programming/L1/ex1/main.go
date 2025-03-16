ae04681973396d99086ccff53ca103d034255b14
Pawel <grzegory.pawel@gmail.com>
2025-03-16 23:20:56 +0100

    Paraller L1

---
+++ semester_4/Paraller Programming/L1/ex1/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+const (
+	NrOfTravelers = 15 // k
+	MinSteps      = 10
+	MaxSteps      = 100
+	MinDelay      = 10 * time.Millisecond // 0.01 sekundy
+	MaxDelay      = 50 * time.Millisecond // 0.05 sekundy
+	BoardWidth    = 15                    // m
+	BoardHeight   = 15                    // n
+)
+
+type Traveler struct {
+	ID     int
+	X, Y   int
+	Symbol string
+}
+
+func (t *Traveler) Move(steps int, ch chan string) {
+	for i := 0; i < steps; i++ {
+		// Losowe opóźnienie
+		delay := MinDelay + time.Duration(rand.Int63n(int64(MaxDelay-MinDelay)))
+		time.Sleep(delay)
+
+		// Losowy kierunek ruchu
+		direction := rand.Intn(4)
+		switch direction {
+		case 0: // W górę
+			t.Y = (t.Y - 1 + BoardHeight) % BoardHeight
+		case 1: // W dół
+			t.Y = (t.Y + 1) % BoardHeight
+		case 2: // W lewo
+			t.X = (t.X - 1 + BoardWidth) % BoardWidth
+		case 3: // W prawo
+			t.X = (t.X + 1) % BoardWidth
+		}
+
+		// Przekazanie informacji o kroku do kanału
+		ch <- fmt.Sprintf("%d %d %d %d %s", time.Now().UnixNano(), t.ID, t.X, t.Y, t.Symbol)
+	}
+}
+
+func Printer(ch chan string, done chan bool) {
+	for msg := range ch { // Pętla działa, dopóki kanał jest otwarty
+		fmt.Println(msg) // Wypisanie komunikatu
+	}
+	done <- true // Sygnalizacja zakończenia pracy
+}
+
+func main() {
+
+	// Kanał do komunikacji z Printerem
+	ch := make(chan string)
+	done := make(chan bool)
+
+	// Uruchomienie Printera jako goroutine
+	go Printer(ch, done)
+
+	// Tworzenie podróżników
+	for i := 0; i < NrOfTravelers; i++ {
+		traveler := Traveler{
+			ID:     i,
+			X:      rand.Intn(BoardWidth),
+			Y:      rand.Intn(BoardHeight),
+			Symbol: fmt.Sprintf("T%d", i),
+		}
+		steps := MinSteps + rand.Intn(MaxSteps-MinSteps+1)
+		go traveler.Move(steps, ch) // Uruchomienie podróżnika jako goroutine
+	}
+
+	// Oczekiwanie na zakończenie wszystkich podróżników
+	time.Sleep(5 * time.Second) // Przykładowe opóźnienie
+	close(ch)                   // Zamknięcie kanału
+	<-done                      // Oczekiwanie na zakończenie pracy Printera
+}
